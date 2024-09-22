3cba597c3bc351605e44931020f9a545b35c56d8
Hian Almeida <hian.almeida@lavid.ufpb.br>
2024-09-22 15:54:43 -0300

    telas de redefinir senha e criar nova conta

--- front-end/src/DefinedRoutes.tsx
+++ front-end/src/DefinedRoutes.tsx
@@ -8,11 +8,14 @@ import AppBarComponent from "./AppBarComponent";
 
 const DefinedRoutes = () => {
     const location = useLocation();
+    const path_name = ['/', '/redefinir_senha', '/novo_user']
     return (
         <div>
-            {location.pathname !== '/' && <AppBarComponent />}
+            {!path_name.includes(location.pathname) && <AppBarComponent />}
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/" element={<Login pageName="Login" buttonText="Acessar" seeOptions={true} />} />
+                <Route path="/redefinir_senha" element={<Login pageName="Redefinir Senha" buttonText="Salvar" seeOptions={false} />} />
+                <Route path="/novo_user" element={<Login pageName="Cadastro de usuário" buttonText="Salvar" seeOptions={false} />} />
                 <Route path="/historico" element={<Historico />} />
                 <Route path="/home" element={<Home />} />
             </Routes>
---
+++ front-end/src/components/ButtonStyled.tsx
@@ -0,0 +1,81 @@
+import { Button as BaseButton } from '@mui/base/Button';
+import { styled } from '@mui/system';
+
+interface ButtonProps {
+    buttonText: string;
+}
+
+export default function ButtonStyled(props: ButtonProps) {
+    return (
+        <Button>{props.buttonText}</Button>
+    )
+};
+
+// estilização do material ui
+const green = {
+    100: "#c8e6c9",
+    200: "#a5d6a7",
+    300: "#81c784",
+    400: "#66bb6a",
+    500: "#689f38",
+    600: "#43a047",
+    700: "#388e3c",
+    800: "#2e7d32",
+    900: "#1b5e20"
+  }
+  
+  const grey = {
+    50: '#F3F6F9',
+    100: '#E5EAF2',
+    200: '#DAE2ED',
+    300: '#C7D0DD',
+    400: '#B0B8C4',
+    500: '#9DA8B7',
+    600: '#6B7A90',
+    700: '#434D5B',
+    800: '#303740',
+    900: '#1C2025',
+  };
+  
+  const Button = styled(BaseButton)(
+    ({ theme }) => `
+    width: 306px;
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-weight: 600;
+    font-size: 0.875rem;
+    line-height: 1.5;
+    background-color: ${green[500]};
+    padding: 8px 16px;
+    border-radius: 8px;
+    color: white;
+    transition: all 150ms ease;
+    cursor: pointer;
+    border: 1px solid ${green[500]};
+    box-shadow: 0 2px 1px ${theme.palette.mode === 'dark' ? 'rgba(0, 0, 0, 0.5)' : 'rgba(45, 45, 60, 0.2)'
+      }, inset 0 1.5px 1px ${green[400]}, inset 0 -2px 1px ${green[600]};
+  
+    &:hover {
+      background-color: ${green[600]};
+    }
+  
+    &:active {
+      background-color: ${green[700]};
+      box-shadow: none;
+      transform: scale(0.99);
+    }
+  
+    &:focus-visible {
+      box-shadow: 0 0 0 4px ${theme.palette.mode === 'dark' ? green[300] : green[200]};
+      outline: none;
+    }
+  
+    &.base--disabled {
+      background-color: ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+      color: ${theme.palette.mode === 'dark' ? grey[200] : grey[700]};
+      border: 0;
+      cursor: default;
+      box-shadow: none;
+      transform: scale(1);
+    }
+  `,
+  );
